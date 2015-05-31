a7804fba1a2bf4d6df888a508b35a700bb95dd5c
loicbourgois <loicbourgois.web@gmail.com>
2015-05-31 20:02:42 +0200

    Choose between command line and conf file

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "brain-generator.hpp"
 #include "util.hpp"
 #include <QCoreApplication>
 
@@ -7,7 +8,23 @@ int main(int argc, char *argv[])
     // Init
     QCoreApplication a(argc, argv);
     Util::init();
-
+    QString command;
+    // Command from terminal or conf.xml ?
+    if(argc > 1)
+    {
+        for(int i = 1 ; i < argc ; i++)
+        {
+            if(i-1)
+            {
+                command += " ";
+            }
+            command += argv[i];
+        }
+    }
+    else
+    {
+        command = Util::getLineFromConf("command");
+    }
     // The end
     return 0;
 }
