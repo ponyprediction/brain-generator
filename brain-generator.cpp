7240c5405265d893a918ed7be43cb1b8cc260681
loicbourgois <loicbourgois.web@gmail.com>
2015-06-01 15:45:11 +0200

    Write brain to file.

--- brain-generator.cpp
+++ brain-generator.cpp
@@ -1,14 +1,42 @@
 #include "brain-generator.hpp"
 #include "util.hpp"
+#include <QJsonObject>
+#include <QFileInfo>
+#include <QJsonDocument>
 
 
 void BrainGenerator::generate(const QString &command)
 {
     // Init
     bool ok = true;
+    QFile file;
+    QString filename;
     //
     if(ok)
     {
+        filename = Util::getLineFromConf("brainFilename", &ok);
+        filename.replace("NAME", "bob");
+    }
+    // Open up JSON ..
+    if(ok)
+    {
+        file.setFileName(filename);
+        if (!file.open(QFile::WriteOnly))
+        {
+            ok = false;
+            Util::writeError("cannot open file "
+                             + QFileInfo(file).absoluteFilePath());
+        }
+    }
+    // Write down Json
+    if(ok)
+    {
+        QJsonDocument document;
+        QJsonObject brain;
 
+        brain["ratio"] = 0.0;
+
+        document.setObject(brain);
+        file.write(document.toJson());
     }
 }
